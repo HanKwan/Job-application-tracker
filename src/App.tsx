cec58321683079cc1e261512014f95fc3cb53822
Han Kwan Saing <saihankwansaing@gmail.com>
2026-04-14 12:01:46 +0900

    remove: removed context

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { AppliedProvider } from './contexts/AppliedApplicationContext'
 import './css/App.css'
 import Home from './pages/Home'
 
 function App() {
 
   return (
-    <AppliedProvider>
       <Home />
-    </AppliedProvider>
-
   )
 }
 
