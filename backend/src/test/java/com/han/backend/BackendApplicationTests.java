188ee14f471be3ba7958f573325c2efcc434a562
Han Kwan Saing <saihankwansaing@gmail.com>
2026-06-15 03:36:07 +0900

    initialized spring boot folder for backend for job tracker

---
+++ backend/src/test/java/com/han/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.han.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
